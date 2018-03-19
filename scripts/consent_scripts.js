dd5bfa1ca1538fc6914c0a7660954e581aab53fb
Luxapodular <luca.damasco@gmail.com>
2018-03-19 18:10:08 -0400

    Added consent page functionality.

---
+++ scripts/consent_scripts.js
@@ -0,0 +1,10 @@
+function continueToTask() {
+  var ageElem = document.getElementById("age-consent-yes");
+  var readElem = document.getElementById("read-consent-yes");
+  var participateElem = document.getElementById("participate-consent-yes");
+  
+  console.log(ageElem.value); 
+  console.log(readElem.value); 
+  console.log(participateElem.value); 
+  
+}
