83f5e70912488a9db1a2a7460504132f20d04baf
Luxapodular <luca.damasco@gmail.com>
2018-03-19 21:28:20 -0400

    Added proper logging to completion. Added error text if no user id is present

--- scripts/post_scripts.js
+++ scripts/post_scripts.js
@@ -2,6 +2,12 @@ updateID();
 
 function updateID() {
   var elem = document.getElementById("submission-code");
-  elem.innerHTML = getUserID();
-  postToSheet("Complete-Task", "N/A", "END");
+  var id = getUserID();
+  
+  if (id) {
+    elem.innerHTML = id; 
+    postToSheet("Complete-Task", "N/A", "END");
+  } else {
+    postToSheet("Complete-Task (NO ID)", "N/A", "END");
+  }
 }
