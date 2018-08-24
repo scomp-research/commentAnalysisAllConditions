a27cc79d7ba61a342ec5164665dfa533bc552b8f
scomp-research <37495080+scomp-research@users.noreply.github.com>
2018-08-24 14:35:57 -0400

    Add files via upload

---
+++ scripts/captcha.js
@@ -0,0 +1,47 @@
+success = [1,2,5,6,8]; // Positions of objects that are okay to select.
+selected = []; // Positions of objects that are selected at time of validation.
+
+function validation() {
+  $('.captcha-cell').each(function(i, cell) {
+    if (cell.classList.contains("selected")) {
+      selected.push(i+1); 
+    }
+  });
+  
+  var fail = false; 
+  selected.forEach(function(i) {
+    if (!success.includes(i)) {
+      fail = true; 
+    }  
+  }); 
+
+  if (fail) {
+    alert("Please select images of faces."); 
+    selected = []; 
+  } else {
+    parent.validateIntervention("faces-LowNeg", selected); 
+  }
+}
+
+function initialize() {
+  $('.captcha-cell').click(function () {
+    var icon = $(this).find(".marked-icon");
+    
+    if (icon.css("display")==="block") {
+      icon.css("display", "none");
+      $(this).removeClass("selected"); 
+    } else {
+      icon.css("display", "block");
+      $(this).addClass("selected"); 
+    }
+  });
+}
+
+$(document).ready(function() {
+    initialize(); 
+});
+
+
+
+
+
